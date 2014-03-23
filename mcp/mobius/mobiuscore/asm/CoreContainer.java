7d43d310ef35133fe70dd3b5128b0ab7a8ca2c11
ProfMobius <unknown>
2014-03-23 14:03:18 +0100

    Version bump

--- mcp/mobius/mobiuscore/asm/CoreContainer.java
+++ mcp/mobius/mobiuscore/asm/CoreContainer.java
@@ -17,7 +17,7 @@ public class CoreContainer extends DummyModContainer {
 		ModMetadata md = getMetadata();
 		md.modId   = "MobiusCore";
 		md.name    = "MobiusCore";
-		md.version = "1.1.0_dev2";
+		md.version = "1.1.0_dev3";
 		md.credits = "ProfMobius";
 		md.authorList = Arrays.asList("ProfMobius");
 		md.description = "";
